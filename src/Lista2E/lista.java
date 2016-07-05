1370376b78ea90bba49033711bc0d0da0e052afe
Pau <Pau@10.56.181.145>
2016-07-05 16:13:25 -0600

    inicial

---
+++ src/Lista2E/lista.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lista2E;
+
+/**
+ *
+ * @author Pau
+ */
+public class lista {
+    nodo inicio,fin;
+    public lista (){
+        inicio=null;
+        fin=null;
+    }
+    public void insertarP(int cantidad, String producto, nodo siguiente){
+        
+    }
+}
